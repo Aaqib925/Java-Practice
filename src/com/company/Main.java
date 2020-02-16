dfea45df186fee1a4e182f5b1cb92c30370939dc
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-16 15:38:01 +0500

    practicing

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I don't know If I can do with java");
+        System.out.println("How should I code, Please someone help me");
+        System.out.println("The things here are quite different from Python");
+    }
+}
