84c11dac8d8dd6afaa2a54a87dc9bba758a9e77f
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-20 19:45:05 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,8 +12,14 @@ package com.company;
 
 public class Main{
     public static void main(String[] args) {
-        int age = 30;
-        System.out.println(age);
-
+//        long age = 30;
+//        age = 35;
+//        System.out.println(age);
+//        age = 3_123_456_789L;   // as default java considers a number as integer, if there is number out of range
+        // we have to use long as L at the last of number
+//        System.out.println(age);
+        // by default jave has the decimal point number as double, for float use suffix f
+//        float age = 10.56f;
+//        System.out.println(age);
     }
 }
