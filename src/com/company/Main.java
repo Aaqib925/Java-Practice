8e639a4c4762ff9dbe5f25e9861d947cd88ed5ac
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-20 19:36:36 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        System.out.println("I don't know If I can do with java");
+//        System.out.println("How should I code, Please someone help me");
+//        System.out.println("The things here are quite different from Python");
+//    }
+//}
 
-public class Main {
-
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("I don't know If I can do with java");
-        System.out.println("How should I code, Please someone help me");
-        System.out.println("The things here are quite different from Python");
+        int age = 30;
+        System.out.println(age);
+
     }
 }
