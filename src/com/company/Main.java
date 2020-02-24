0ee535e525108e5c79d7dcbdbaa7fd17469e96a1
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-24 22:54:20 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -228,3 +228,15 @@ import java.util.Scanner;
 //    }
 //}
 
+public class Main{
+    public static void main(String[] args){
+        System.out.print("Enter any number: ");
+        int number = new Scanner(System.in).nextInt();
+        if (number % 2 == 0){
+            System.out.println("The number " + number + " is Even.");
+        }
+        else{
+            System.out.println("The number " + number + " is Odd");
+        }
+    }
+}
