9995dac8885f6465ea7f85977a00ba5a11ba52e6
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-26 16:41:25 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -376,12 +376,12 @@ import java.util.Scanner;
 //    }
 //}
 
-public class Main{
-    public static void main(String[] args){
-        int i = 0;
-        while (i < 5){
-            System.out.println("Hello World");
-            i++;
-        }
-    }
-}
+//public class Main{
+//    public static void main(String[] args){
+//        int i = 0;
+//        while (i < 5){
+//            System.out.println("Hello World");
+//            i++;
+//        }
+//    }
+//}
