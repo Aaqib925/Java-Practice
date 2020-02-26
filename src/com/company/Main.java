8dc841730d177eab31f950f46af38fb84243ede9
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-26 16:45:52 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -385,3 +385,22 @@ import java.util.Scanner;
 //        }
 //    }
 //}
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+//        while (!input.equals("quit")){
+//            System.out.print("Input: ");
+//            input = scanner.next().toLowerCase();
+//            System.out.println(input);
+//        }
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+
+    }
+}
