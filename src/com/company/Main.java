90c690bd8cf88daf06849cd7c9ce292079906f9a
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-26 16:54:03 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -407,11 +407,15 @@ import java.util.Scanner;
 
 // for loop used for iterations
 
-public class Main{
-    public static void main(String[] args){
-        String[] fruits = { "Apple", "Mango", "Banana" };
-        for (int i = 0; i < fruits.length; i ++){
-            System.out.println(fruits[i]);
-        }
-    }
-}
+//public class Main{
+//    public static void main(String[] args){
+//        String[] fruits = { "Apple", "Mango", "Banana" };
+//        for (int i = 0; i < fruits.length; i ++){
+//            System.out.println(fruits[i]);
+//        }
+
+//        for (String i: fruits){
+//            System.out.println(i);
+//        }
+//    }
+//}
