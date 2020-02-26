6283dc6d82104b5a274f2b5abf4b4f9d0a886434
aaqib925 <muhammadaaqib925@gmail.com>
2020-02-26 15:59:54 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -354,3 +354,12 @@ import java.util.Scanner;
 //        }
 //    }
 //}
+
+// for loops
+public class Main{
+    public static void main(String[] args){
+        for (int i = 0; i < 5; i++){
+            System.out.println("Aaqib");
+        }
+    }
+}
