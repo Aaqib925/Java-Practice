c12ade3274c77df88fb975eeca5c92ab9b78eda0
aaqib925 <muhammadaaqib925@gmail.com>
2020-03-09 14:40:44 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -537,7 +537,7 @@ public class Main{
         int num = scanner.nextInt();
         for (int i = 1; i <= num; i ++){
             for (int j = 1; j <= i; j ++){
-                System.out.print("*");
+                System.out.print(j);
             }
             System.out.println("");
         }
