3f13fb6bfb18278c0719e2d4f80e04a6d9500042
aaqib925 <muhammadaaqib925@gmail.com>
2020-03-09 14:35:47 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -523,10 +523,10 @@ import java.util.Scanner;
 //}
 
 public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter any number: ");
-        int number = scanner.nextInt();
-        System.out.println(number);
-    }
-}
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter any number: ");
+//        int number = scanner.nextInt();
+//        System.out.println(number);
+//    }
+//}
