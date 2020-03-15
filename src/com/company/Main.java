3a2abbc4853f23083fe5c9dd54b60524c442e5ed
aaqib925 <muhammadaaqib925@gmail.com>
2020-03-15 10:11:18 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -543,4 +543,19 @@ import java.util.Scanner;
 //        }
 //    }
 //}
+// Creating a dynamic array
 
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of your array: ");
+        int size = scanner.nextInt();
+        int[] myArray = new int[size];
+        for (int i = 0; i < size; i ++){
+            System.out.print("Enter the value for index " + i + ": ");
+            int x = scanner.nextInt();
+            myArray[i] = x;
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
+}
