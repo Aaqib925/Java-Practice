dc58b3b89a89393fa81cd84ea8875090695c65e1
aaqib925 <muhammadaaqib925@gmail.com>
2020-04-08 00:53:44 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -585,15 +585,15 @@ import java.util.*;
 //    }
 //}
 
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter any number: ");
-        int number = scanner.nextInt();
-        for (int i = 1; i <= number; i++){
-            if (number % i == 0){
-                System.out.println(i + " is the factor of " + number);
-            }
-        }
-    }
-}
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter any number: ");
+//        int number = scanner.nextInt();
+//        for (int i = 1; i <= number; i++){
+//            if (number % i == 0){
+//                System.out.println(i + " is the factor of " + number);
+//            }
+//        }
+//    }
+//}
