d0c982ddc2ae3f975d8e335969195893b7f34ab7
aaqib925 <muhammadaaqib925@gmail.com>
2020-04-09 00:44:16 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -687,13 +687,13 @@ import java.util.*;
 //    }
 //}
 
-public class Main{
-    public static void main(String[] args){
-        printFunction("Aaqib");
-    }
-    // creating a greeting static function, a static method is related to the class not for object
-    // void means it doesn't return anything
-    public static void printFunction(String name){
-        System.out.println("Hello " + name);
-    }
-}
+//public class Main{
+//    public static void main(String[] args){
+//        printFunction("Aaqib");
+//    }
+//    // creating a greeting static function, a static method is related to the class not for object
+//    // void means it doesn't return anything
+//    public static void printFunction(String name){
+//        System.out.println("Hello " + name);
+//    }
+//}
