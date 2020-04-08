3259813b529144154ea2805521dd0b5e042a8f88
aaqib925 <muhammadaaqib925@gmail.com>
2020-04-08 15:00:53 +0500

    practicing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -644,3 +644,12 @@ import java.util.*;
 ////        }
 //    }
 //}
+
+//public class Main{
+//    public static void main(String[] args){
+//        int[][] grades = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        for (int[] x: grades){
+//            System.out.println(Arrays.toString(x));
+//        }
+//    }
+//}
